030f147b9150424c88785c52763cdaa62e677bca
RashiniK <162806612+RashiniK@users.noreply.github.com>
2024-09-16 20:15:32 -0300

    primeiro comit

---
+++ index.php
@@ -0,0 +1,25 @@
+
+ 
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Acessar</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Acessar</h2>
+            <div class="form-group">
+                <a href ="login.php" ><button type="button" class="btn btn-primary btn-lg btn-block" >Login</button></a>
+            </div>    
+            <div class="form-group">
+                <a href ="vizualizartds.php" ><button type="button" class="btn btn-primary btn-lg btn-block" >Visualizar todas solicitações</button></a>
+            </div>
+    </div>    
+</body>
+</html>
